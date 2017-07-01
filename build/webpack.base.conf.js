f99ae97ac6e86ff90f2b46f11f0911e570808759
CheolminConanShin <scm0226@gmail.com>
2017-07-01 11:17:48 +0900

    GOAL: Removed byte limit for font - Font files exceeding byte limit were unable to use

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -49,7 +49,6 @@ module.exports = {
         test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/,
         loader: 'url-loader',
         options: {
-          limit: 10000,
           name: utils.assetsPath('fonts/[name].[ext]')
         }
       }
