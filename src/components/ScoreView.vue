53865ffb873becf0b752e57b511a66acbf427b7e
CheolminConanShin <scm0226@gmail.com>
2017-07-01 12:23:18 +0900

    GOAL: Introduced buy coffee button

--- src/components/ScoreView.vue
+++ src/components/ScoreView.vue
@@ -6,6 +6,7 @@
     <score-row></score-row>
     <score-row></score-row>
     <score-row></score-row>
+    <div class="buy-coffee">나만 아니면 돼</div>
   </div>
 </template>
 <script>
@@ -37,5 +38,18 @@
     width: 55vw;
     height: 5.5vh;
     margin-bottom: 5vh;
+  }
+
+  .buy-coffee {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    width: 70vw;
+    height: 10vh;
+    border-radius: 20vw;
+    background-color: rgba(242, 187, 5, 0.3);
+    margin: 0px 10vw;
+    font-size: 3vh;
+    color: white;
   }
 </style>
