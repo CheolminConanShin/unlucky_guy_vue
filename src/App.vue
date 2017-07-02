98e872792e0853c20657542e0f4c7b233907150d
CheolminConanShin <scm0226@gmail.com>
2017-07-02 18:06:33 +0900

    GOAL: Removed font style because the size is too large

--- src/App.vue
+++ src/App.vue
@@ -11,18 +11,13 @@
 </script>
 
 <style>
-  @font-face {
-    font-family: NanumBarunGothic;
-    src: url("./assets/fonts/NanumBarunGothic.eot"), url("./assets/fonts/NanumBarunGothic.ttf"), url("./assets/fonts/NanumBarunGothic.woff"), url("./assets/fonts/NanumBarunGothic.woff2")
-  }
-
   html, body, #app {
     height: 100%;
     width: 100%;
   }
 
   #app {
-    font-family: NanumBarunGothic;
+    font-family: sans-serif;
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
     text-align: center;
